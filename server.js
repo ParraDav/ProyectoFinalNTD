e96df2e4b5d43a27ea14017c2496a17a0f9782b1
Steven <Pospinachico@gmail.com>
2026-04-27 20:12:48 -0500

    Creación de nueva clase y ruta
    Se crea clase authRoutes y se crea la ruta en server.js

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+// Middleware
 app.use(express.json());
+
+// Rutas
+app.use("/api/auth", require("./routes/authRoutes"));
 
 app.listen(3000, () => {
     console.log("Servidor corriendo");
