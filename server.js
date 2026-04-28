24a33b394f6950d8fdf36bc2f1a9dddc274b310e
Steven <Pospinachico@gmail.com>
2026-04-27 19:32:29 -0500

    Mongoooo db
    Conexión a MongoDB usando process.env.MONGO_URI

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
+
 app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Servidor corriendo");
 });
+mongoose.connect(process.env.MONGO_URI)
