e01d55c8933f6a4f23fd8351fd2d19bea830318e
Steven <Pospinachico@gmail.com>
2026-04-27 20:29:56 -0500

    Modificacion de server para que indique error de conexion
    Se implementa flujo de arranque: conexión a MongoDB con Mongoose y levantamiento del servidor tras conexión exitosa

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 // Middleware
 app.use(express.json());
+app.use(cors()); 
 
 // Rutas
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/cursos", require("./routes/cursoRoutes"));
 
 //Conexion con MongoDB
-app.listen(3000, () => {
-    console.log("Servidor corriendo");
-});
-mongoose.connect(process.env.MONGO_URI).then(() => console.log("Conectado a MongoDB"))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Conectado a MongoDB");
+
+    app.listen(process.env.PORT, () => {
+        console.log("Servidor corriendo en puerto " + process.env.PORT);
+    });
+})
+.catch(err => console.log(err));
